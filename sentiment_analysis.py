3849392adbf09da60b20daba94450a286f531f3c
SHESHAL01 <chamudithasheshal12@gmail.com>
2026-03-10 19:13:44 +0530

    Convert text to numbers(vectorization) and trained the model

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -3,6 +3,9 @@ import pandas as pd
 import nltk
 import re
 from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
 
 # Preprocessing
 data = pd.read_csv("IMDB Dataset.csv")
@@ -26,3 +29,15 @@ def clean_text(text):
     return " ".join(words)
 
 data['clean_text'] = data['review'].apply(clean_text)
+
+# perform vectorization
+vectorizer = TfidfVectorizer()
+
+X = vectorizer.fit_transform(data['clean_text'])
+y = data['sentiment']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# model training
+model = MultinomialNB()
+model.fit(X_train, y_train)
