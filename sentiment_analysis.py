f9a223fdd39be029f081ca524fa329e4b1354318
SHESHAL01 <chamudithasheshal12@gmail.com>
2026-03-12 20:00:42 +0530

    finalizing core functionalities

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -6,6 +6,7 @@ from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
 
 # Preprocessing
 data = pd.read_csv("IMDB Dataset.csv")
@@ -41,3 +42,16 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 # model training
 model = MultinomialNB()
 model.fit(X_train, y_train)
+
+predictions = model.predict(X_test)
+
+print("Accuracy:", accuracy_score(y_test, predictions))
+
+def predict_sentiment(text):
+    text = clean_text(text)
+    vector = vectorizer.transform([text])
+    prediction = model.predict(vector)
+    return prediction[0]
+
+print("Review Status:")
+print(predict_sentiment("If you watch it like \"Not Sherlock\" its a good show If you watch it like \"Sherlock\" its not.Sherlocks intellectual and deduction capacity is not at its full potential and can be reflected much better. Mycroft is completely different which actually he s capabilities are also extra ordinary but this version is very ordinary.Dont compare with Sherlock and enjoy the show as its a different tv series."))
